f7f409843dd536e328edf850646b9beb29e0751d
OnLiU <public.danuel@gmail.com>
2019-03-20 00:48:57 +0900

    change useCSSVariable default action

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import { useCSSVariableAsNumberByRef } from './useCSSVariableAsNumberByRef'
 import { useCSSVariableAsPercentByRef } from './useCSSVariableAsPercentByRef'
 import { useCSSVariableAsRGBAByRef } from './useCSSVariableAsRGBAByRef'
 import { useCSSVariableAsRGBByRef } from './useCSSVariableAsRGBByRef'
-import { useCSSVariableByRef } from './useCSSVariableByRef'
+import { useCSSVariableAsStringByRef } from './useCSSVariableAsStringByRef'
 
 export { useCSSVariableAsNumberByRef } from './useCSSVariableAsNumberByRef'
 export { useCSSVariableAsPercentByRef } from './useCSSVariableAsPercentByRef'
 export { useCSSVariableAsRGBAByRef } from './useCSSVariableAsRGBAByRef'
 export { useCSSVariableAsRGBByRef } from './useCSSVariableAsRGBByRef'
-export { useCSSVariableByRef } from './useCSSVariableByRef'
+export { useCSSVariableAsStringByRef } from './useCSSVariableAsStringByRef'
+export { useCSSVariableAsStringByRef as useCSSVariableByRef } from './useCSSVariableAsStringByRef'
 
 export const useCSSVariableAsNumber = factoryRoot(useCSSVariableAsNumberByRef)
 export const useCSSVariableAsPercent = factoryRoot(useCSSVariableAsPercentByRef)
 export const useCSSVariableAsRGBA = factoryRoot(useCSSVariableAsRGBAByRef)
 export const useCSSVariableAsRGB = factoryRoot(useCSSVariableAsRGBByRef)
-export const useCSSVariable = factoryRoot(useCSSVariableByRef)
+export const useCSSVariableAsString = factoryRoot(useCSSVariableAsStringByRef)
+export const useCSSVariable = factoryRoot(useCSSVariableAsStringByRef)
--- src/useCSSVariableAsStringByRef.ts
+++ src/useCSSVariableAsStringByRef.ts
@@ -1,21 +1,18 @@
 import { RefObject, useState } from 'react'
 
-export const useCSSVariableByRef = <Element extends HTMLElement>(
+export const useCSSVariableAsStringByRef = <Element extends HTMLElement>(
   ref: RefObject<Element>,
   initialName: string,
-  initialValue?: string | (() => string)
+  initialValue: string | (() => string) = ''
 ): [string, (value: string) => void] => {
   const [name] = useState(() => `--${initialName}`)
   const [style] = useState(() => ref.current!.style)
 
   const updateCSSVariable = (value: string): void => style.setProperty(name, value)
   const [value, setValue] = useState<string>(() => {
-    if (initialValue !== undefined) {
-      const value = typeof initialValue === 'string' ? initialValue : initialValue()
-      updateCSSVariable(value)
-      return value
-    }
-    return style.getPropertyValue(name)
+    const value = typeof initialValue === 'string' ? initialValue : initialValue()
+    updateCSSVariable(value)
+    return value
   })
 
   const updateValue = (value: string) => {
